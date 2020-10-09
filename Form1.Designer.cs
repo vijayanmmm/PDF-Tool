2d70fc8b13c135eaaf922d3e42d8cfba920d9a9d
v0v00gu <v0v00gu@cn.wal-mart.com>
2020-10-09 17:55:18 +0800

    Added version 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,13 +37,27 @@
             this.numPictureQuality = new System.Windows.Forms.NumericUpDown();
             this.btnBrowsePDFFolder = new System.Windows.Forms.Button();
             this.btnBrowseResultImagesFolder = new System.Windows.Forms.Button();
+            this.btnQRCodereading = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.mf_btnBrowseFapiao = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mf_txtFapiaoFolder = new System.Windows.Forms.TextBox();
+            this.dgFapiao = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mf_lblFapiaoTotalAmt = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numPictureQuality)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFapiao)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 56);
+            this.label1.Location = new System.Drawing.Point(48, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 13);
             this.label1.TabIndex = 0;
@@ -51,7 +65,7 @@
             // 
             // txtPDFFolderPath
             // 
-            this.txtPDFFolderPath.Location = new System.Drawing.Point(177, 56);
+            this.txtPDFFolderPath.Location = new System.Drawing.Point(142, 39);
             this.txtPDFFolderPath.Name = "txtPDFFolderPath";
             this.txtPDFFolderPath.Size = new System.Drawing.Size(345, 20);
             this.txtPDFFolderPath.TabIndex = 1;
@@ -59,7 +73,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 108);
+            this.label2.Location = new System.Drawing.Point(7, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(128, 13);
             this.label2.TabIndex = 2;
@@ -67,14 +81,14 @@
             // 
             // txtResultImageFileFolder
             // 
-            this.txtResultImageFileFolder.Location = new System.Drawing.Point(177, 108);
+            this.txtResultImageFileFolder.Location = new System.Drawing.Point(142, 91);
             this.txtResultImageFileFolder.Name = "txtResultImageFileFolder";
             this.txtResultImageFileFolder.Size = new System.Drawing.Size(345, 20);
             this.txtResultImageFileFolder.TabIndex = 3;
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(264, 176);
+            this.btnConvert.Location = new System.Drawing.Point(229, 159);
             this.btnConvert.Name = "btnConvert";
             this.btnConvert.Size = new System.Drawing.Size(198, 23);
             this.btnConvert.TabIndex = 4;
@@ -85,7 +99,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(86, 149);
+            this.linkLabel1.Location = new System.Drawing.Point(51, 132);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(78, 13);
             this.linkLabel1.TabIndex = 5;
@@ -94,7 +108,7 @@
             // 
             // numPictureQuality
             // 
-            this.numPictureQuality.Location = new System.Drawing.Point(177, 149);
+            this.numPictureQuality.Location = new System.Drawing.Point(142, 132);
             this.numPictureQuality.Maximum = new decimal(new int[] {
             480,
             0,
@@ -116,7 +130,7 @@
             // 
             // btnBrowsePDFFolder
             // 
-            this.btnBrowsePDFFolder.Location = new System.Drawing.Point(529, 52);
+            this.btnBrowsePDFFolder.Location = new System.Drawing.Point(494, 35);
             this.btnBrowsePDFFolder.Name = "btnBrowsePDFFolder";
             this.btnBrowsePDFFolder.Size = new System.Drawing.Size(75, 23);
             this.btnBrowsePDFFolder.TabIndex = 7;
@@ -126,7 +140,7 @@
             // 
             // btnBrowseResultImagesFolder
             // 
-            this.btnBrowseResultImagesFolder.Location = new System.Drawing.Point(528, 108);
+            this.btnBrowseResultImagesFolder.Location = new System.Drawing.Point(493, 91);
             this.btnBrowseResultImagesFolder.Name = "btnBrowseResultImagesFolder";
             this.btnBrowseResultImagesFolder.Size = new System.Drawing.Size(75, 23);
             this.btnBrowseResultImagesFolder.TabIndex = 8;
@@ -134,28 +148,133 @@
             this.btnBrowseResultImagesFolder.UseVisualStyleBackColor = true;
             this.btnBrowseResultImagesFolder.Click += new System.EventHandler(this.btnBrowseResultImagesFolder_Click);
             // 
+            // btnQRCodereading
+            // 
+            this.btnQRCodereading.Location = new System.Drawing.Point(464, 145);
+            this.btnQRCodereading.Name = "btnQRCodereading";
+            this.btnQRCodereading.Size = new System.Drawing.Size(105, 64);
+            this.btnQRCodereading.TabIndex = 9;
+            this.btnQRCodereading.Text = "QR Code reading";
+            this.btnQRCodereading.UseVisualStyleBackColor = true;
+            this.btnQRCodereading.Click += new System.EventHandler(this.btnQRCodereading_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(617, 328);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnConvert);
+            this.tabPage1.Controls.Add(this.btnQRCodereading);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnBrowseResultImagesFolder);
+            this.tabPage1.Controls.Add(this.txtPDFFolderPath);
+            this.tabPage1.Controls.Add(this.btnBrowsePDFFolder);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.numPictureQuality);
+            this.tabPage1.Controls.Add(this.txtResultImageFileFolder);
+            this.tabPage1.Controls.Add(this.linkLabel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(609, 237);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Convert PDF to Image";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.mf_lblFapiaoTotalAmt);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.dgFapiao);
+            this.tabPage2.Controls.Add(this.mf_btnBrowseFapiao);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.mf_txtFapiaoFolder);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(609, 302);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Manage Fapiao";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // mf_btnBrowseFapiao
+            // 
+            this.mf_btnBrowseFapiao.Location = new System.Drawing.Point(492, 6);
+            this.mf_btnBrowseFapiao.Name = "mf_btnBrowseFapiao";
+            this.mf_btnBrowseFapiao.Size = new System.Drawing.Size(75, 23);
+            this.mf_btnBrowseFapiao.TabIndex = 11;
+            this.mf_btnBrowseFapiao.Text = "Browse";
+            this.mf_btnBrowseFapiao.UseVisualStyleBackColor = true;
+            this.mf_btnBrowseFapiao.Click += new System.EventHandler(this.mf_btnBrowseFapiao_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Fapiao Folder(IMG/PDF):";
+            // 
+            // mf_txtFapiaoFolder
+            // 
+            this.mf_txtFapiaoFolder.Location = new System.Drawing.Point(141, 6);
+            this.mf_txtFapiaoFolder.Name = "mf_txtFapiaoFolder";
+            this.mf_txtFapiaoFolder.Size = new System.Drawing.Size(345, 20);
+            this.mf_txtFapiaoFolder.TabIndex = 10;
+            // 
+            // dgFapiao
+            // 
+            this.dgFapiao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFapiao.Location = new System.Drawing.Point(9, 64);
+            this.dgFapiao.Name = "dgFapiao";
+            this.dgFapiao.Size = new System.Drawing.Size(594, 232);
+            this.dgFapiao.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Fapiao Total:";
+            // 
+            // mf_lblFapiaoTotalAmt
+            // 
+            this.mf_lblFapiaoTotalAmt.AutoSize = true;
+            this.mf_lblFapiaoTotalAmt.ForeColor = System.Drawing.Color.Maroon;
+            this.mf_lblFapiaoTotalAmt.Location = new System.Drawing.Point(249, 33);
+            this.mf_lblFapiaoTotalAmt.Name = "mf_lblFapiaoTotalAmt";
+            this.mf_lblFapiaoTotalAmt.Size = new System.Drawing.Size(13, 13);
+            this.mf_lblFapiaoTotalAmt.TabIndex = 14;
+            this.mf_lblFapiaoTotalAmt.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 238);
-            this.Controls.Add(this.btnBrowseResultImagesFolder);
-            this.Controls.Add(this.btnBrowsePDFFolder);
-            this.Controls.Add(this.numPictureQuality);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.btnConvert);
-            this.Controls.Add(this.txtResultImageFileFolder);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtPDFFolderPath);
-            this.Controls.Add(this.label1);
-            this.MaximumSize = new System.Drawing.Size(657, 277);
+            this.ClientSize = new System.Drawing.Size(641, 338);
+            this.Controls.Add(this.tabControl1);
             this.MinimumSize = new System.Drawing.Size(657, 277);
             this.Name = "Form1";
             this.Text = "PDF Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numPictureQuality)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFapiao)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -170,6 +289,16 @@
         private System.Windows.Forms.NumericUpDown numPictureQuality;
         private System.Windows.Forms.Button btnBrowsePDFFolder;
         private System.Windows.Forms.Button btnBrowseResultImagesFolder;
+        private System.Windows.Forms.Button btnQRCodereading;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button mf_btnBrowseFapiao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox mf_txtFapiaoFolder;
+        private System.Windows.Forms.DataGridView dgFapiao;
+        private System.Windows.Forms.Label mf_lblFapiaoTotalAmt;
+        private System.Windows.Forms.Label label4;
     }
 }
 
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Spire.Pdf;
+using Spire.Pdf;
+using ZXing;
+using ZXing.Magick;
+using ZXing.QrCode;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace PDF_Tool
+{
+    public partial class Form1 : Form
+    {
+        private string strVersion = "1.0";
+        private DataTable tblFapiao = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            foreach (var strFile in System.IO.Directory.GetFiles(txtPDFFolderPath.Text))
+            {
+                convertToPDF(strFile, txtResultImageFileFolder.Text, Convert.ToInt32(numPictureQuality.Value));
+            }
+        }
+
+
+        private void convertToPDF(String strPDFFilePath, string strResultFolderPath, int intDPI)
+        {
+            PdfDocument pdfdocument = new PdfDocument(strPDFFilePath);
+            string strFileName = strPDFFilePath.Substring(strPDFFilePath.LastIndexOf("\\", StringComparison.CurrentCulture) + 1);
+
+            for (int i = 0; i < pdfdocument.Pages.Count; i++)
+            {
+                System.Drawing.Image image = pdfdocument.SaveAsImage(i, intDPI, intDPI);
+                image.Save(strResultFolderPath + "\\" + string.Format(strFileName + "{0}.png", i), System.Drawing.Imaging.ImageFormat.Png);
+            }
+
+        }
+
+        private void convertFirstPageToImg(String strPDFFilePath, string strImgFilePath, int intDPI)
+        {
+            PdfDocument pdfdocument = new PdfDocument(strPDFFilePath);
+            System.Drawing.Image image = pdfdocument.SaveAsImage(0, intDPI, intDPI);
+            image.Save(strImgFilePath, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+
+        private void btnBrowsePDFFolder_Click(object sender, EventArgs e)
+        {
+            txtPDFFolderPath.Text = BrowseFolder();
+        }
+
+        private void btnBrowseResultImagesFolder_Click(object sender, EventArgs e)
+        {
+            txtResultImageFileFolder.Text = BrowseFolder();
+        }
+
+
+        private string BrowseFolder()
+        {
+            FolderBrowserDialog file = new FolderBrowserDialog();
+            DialogResult dr = file.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                return file.SelectedPath;
+            }
+
+            return "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = this.Text + " V" + strVersion;
+
+            //Load the columns
+            tblFapiao.Columns.Add("FileName");
+            tblFapiao.Columns.Add("Type");
+            tblFapiao.Columns.Add("发票代码");
+            tblFapiao.Columns.Add("发票号码");
+            tblFapiao.Columns.Add("不含税金额");
+            tblFapiao.Columns.Add("开票日期");
+            tblFapiao.Columns.Add("校验码");
+            tblFapiao.Columns.Add("随机产生的机密信息");
+            dgFapiao.DataSource = tblFapiao;
+        }
+
+        private void btnQRCodereading_Click(object sender, EventArgs e)
+        {
+
+            string strImgPath = @"C:\PDF\PDF Tool\Data\zoutput53.jpeg";
+            strImgPath = @"C:\PDF\PDF Tool\Data\A_text,_your_name,_a_number_or_anything..._;)_as_QRcode.png";
+
+
+//            01,04（01,04是普通发票，01,01是专用发票）,1200153320（发票代码）,07041662（发票号码）,183.49（不含税金额）,20151221（开票日期）,83623873463907646339（校验码）,5080（随机产生的机密信息）
+
+//作者：西顾
+//链接：https://www.zhihu.com/question/46433405/answer/146524598
+//            来源：知乎
+//            著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+            var reader = new ZXing.Magick.BarcodeReader();
+            reader.Options.PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.QR_CODE};
+
+            using (var image = new  ImageMagick.MagickImage(strImgPath))
+            {
+                var result = reader.Decode(image);
+                if (result != null)
+                {
+                    Console.WriteLine("QR Code read");
+                    Console.WriteLine(result.Text);
+                }
+            }
+
+
+            //QRCodeImage qrimg = new Bitmap(strImgPath);
+            //QRCodeDecoder decode = new QRCodeDecoder();
+            //decode.Decode()
+
+
+
+            //Bitmap img = new Bitmap(strImgPath);
+            //ZXing.Binarizer bin;
+            
+
+
+            //ZXing.BinaryBitmap imgBinary = new ZXing.BinaryBitmap()
+
+            //QRCodeReader qrReader = new QRCodeReader();
+            //ZXing.Result objResult = qrReader.decode(img);
+
+        }
+
+        private void mf_btnBrowseFapiao_Click(object sender, EventArgs e)
+        {
+            mf_btnBrowseFapiao.Text = BrowseFolder();
+            string strDecodedText;
+            string strFapiaoType;
+            float fltFapiaoTotalAmount = 0f;
+            string[] strArrOutput;
+
+            //Read all files in the folder
+            foreach (var strFile in System.IO.Directory.GetFiles(mf_btnBrowseFapiao.Text))
+            {
+                strDecodedText = "";
+                //Check is it PDF of text file
+                if (strFile.Substring(strFile.Length - 4).ToLower() == ".pdf")
+                {
+                    convertFirstPageToImg(strFile, strFile.ToLower().Replace(".pdf",".jpeg"), Convert.ToInt32(numPictureQuality.Value));
+                    strDecodedText = DecodeQRCode(strFile);
+                }
+                else if (strFile.Substring(strFile.Length - 5).ToLower() == ".jpeg")
+                {
+                    strDecodedText = DecodeQRCode(strFile);
+                }
+
+                if (strDecodedText != null && strDecodedText != "" && strDecodedText.Contains("http") == true)
+                {
+                //Scanned result is an URL
+                //Capture the amount from URL , Sample
+                https://bcfp.shenzhen.chinatax.gov.cn/verify/scan?hash=007e07634a22c81eba4c66b5338819870eda751a830a2b12e66cad5296e923b205&bill_num=11427293&total_amount=7990
+                    string strTem = strDecodedText.Substring(strDecodedText.IndexOf("&bill_num"));
+                    strArrOutput = strTem.Split( new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+                    strTem = strArrOutput[0].Replace("bill_num=", "");
+                    float fltAmount = float.Parse(strArrOutput[1].Replace("total_amount=", "")) / 100;
+
+
+
+
+                    tblFapiao.Rows.Add(strFile, "", "", strTem, fltAmount.ToString(), "","","");
+
+                    fltFapiaoTotalAmount = fltFapiaoTotalAmount + fltAmount;
+                }
+                else if (strDecodedText != null && strDecodedText != "")
+                {
+                   strArrOutput = strDecodedText.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries); 
+
+                    //01,04（01,04是普通发票，01,01是专用发票）
+                    if (strArrOutput[1].Trim() == "01,01")
+                        strFapiaoType = "专用发票";
+                    else
+                        strFapiaoType = "普通发票";
+                    
+                    //Add row
+                    tblFapiao.Rows.Add(strFile, strFapiaoType,strArrOutput[2], strArrOutput[3], strArrOutput[4], strArrOutput[5], strArrOutput[6], strArrOutput[7]);
+
+                    //Add amount into total
+                    fltFapiaoTotalAmount = fltFapiaoTotalAmount + float.Parse(strArrOutput[4]);
+
+                    //strArrOutput[7] - dont know what this represents
+                    //01,04（01,04是普通发票，01,01是专用发票）,1200153320（发票代码）,07041662（发票号码）,183.49（不含税金额）,20151221（开票日期）,83623873463907646339（校验码）,5080（随机产生的机密信息）
+                    //作者：西顾
+                    //链接：https://www.zhihu.com/question/46433405/answer/146524598
+                    //来源：知乎
+                    //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+                }
+                else
+                {
+                    tblFapiao.Rows.Add(strFile, "Not valid format");
+                }
+            }
+
+            //Display the results
+            dgFapiao.DataSource = tblFapiao;
+            mf_lblFapiaoTotalAmt.Text = fltFapiaoTotalAmount.ToString();
+        }
+
+
+        private string DecodeQRCode(string strImgFilePath)
+        {
+            var reader = new ZXing.Magick.BarcodeReader();
+            reader.Options.PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.QR_CODE };
+
+            using (var image = new ImageMagick.MagickImage(strImgFilePath))
+            {
+                var result = reader.Decode(image);
+                if (result != null)
+                {                    
+                    return result.Text;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
